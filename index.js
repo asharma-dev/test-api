b06781a7e6584af05abfe4f4acc8c232ae1f682e
abhisheksharma1123 <abhisheksharma1123@gmail.com>
2023-08-21 21:30:19 -0400

    undo

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let checkDB = "waiting for connection";
 require("dotenv").config();
 // Connect to Mongo
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect(process.env.MONGO_URI, {
     useUnifiedTopology: true,
   })
   .then(() => {
